e2c72657da4e4d862223716f45b16a125f72fbfb
Sirichaitra <chaitra.tumpudi@gmail.com>
2026-09-05 09:29:40 +0530

    Delete lab directory

--- lab/exp4_a1.ts
+++
@@ -1,7 +0,0 @@
-export class Student {
-    name: string = "Siri";
-
-    display(): void {
-        console.log(this.name);
-    }
-}
--- lab/exp4_a2.ts
+++
@@ -1,4 +0,0 @@
-import { Student } from "./exp4_a1.js";
-
-let s = new Student();
-s.display();
